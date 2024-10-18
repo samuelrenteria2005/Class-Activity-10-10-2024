f8b5379b43378f1f7d0b0e708ea73e77a2579eb2
Samuel Renteria <srenter4@ucsc.edu>
2024-10-17 22:33:54 -0700

    Class Activity 10-17-2024

---
+++ personal-website.js
@@ -0,0 +1,8 @@
+document.getElementById("myPicture").onclick = function() {
+    alert("Hi");
+};
+
+document.getElementById("messageMe").onclick = function() {
+    alert("haha you thought");
+};
+
